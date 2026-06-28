a79f4d5d899a3725d902665d905e1aa24111dd33
x5red78-HACKC1UB <x5red78@gmail.com>
2026-06-28 04:00:00 +0000

    basic calc done(in one session) now imma watch hells paradise

---
+++ index.js
@@ -0,0 +1,17 @@
+const display=document.getElementById("numberdisplay");
+function appendToDisplay(input) {
+    display.value+= input;
+}
+function clearDisplay() {
+    display.value="";
+}
+function calculate() {
+    
+    try{
+    display.value=eval(display.value);
+    }
+    catch(error){
+        display.value="Error :("
+    }
+}
+    
