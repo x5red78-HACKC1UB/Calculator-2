80d1c3901ef9c1475c668cb58ec782a9f3ca6153
x5red78-HACKC1UB <x5red78@gmail.com>
2026-06-30 04:41:05 +0000

    commit bugs

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 const display=document.getElementById("numberdisplay");
+const calc =document.getElementById("calculator");
+const background =document.getElementById("background");
+
 function appendToDisplay(input) {
     display.value+= input;
-    display.textContent += input;
-display.scrollLeft = numberdisplay.scrollWidth;
+background.textContent=display.value;
+
 }
 function clearDisplay() {
     display.value="";
+   background.textContent="";
+
 }
 function calculate() {
     
     try{
     display.value=eval(display.value);
+   background.textContent=display.value;
     }
     catch(error){
         display.value="Error :("
+        background.textContent=display.value;
     }
-}
-    
-display.textContent += digit;
-display.scrollLeft = numberdisplay.scrollWidth;
+};
+
+document.addEventListener("mousemove", (e) =>{
+const distanceX = (window.innerWidth/2 - e.clientX)/25;
+const distanceY = (window.innerHeight/2 - e.clientY)/25;
+calc.style.transform =`rotateX(${distanceY}deg) rotateY(${-distanceX}deg)`
+});
+  
+
+
+
